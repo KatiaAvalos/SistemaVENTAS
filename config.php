e4b44335f3d50cd46e66e8a15ddb08dfc9306a64
KatiaAvalos <katia.avalos4270@alumnos.udg.mx>
2024-10-29 15:53:44 -0600

    Configuracion

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+define('SERVIDOR', 'localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sistemadeventas');
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+try{
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+   // echo "La conexion a la base de datos fue exitosa";
+}catch(PDOException $e){
+   // print_r($e);
+   echo "ERROR en la conexion de la base de datos";
+}
+$URL = "http://localhost/SistemadeVentas";
+?>
